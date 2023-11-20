2f3254b21f281cf8ca2da50ff091ad325346f8ee
Max Shen <m4xshen@gmail.com>
2023-11-20 23:46:57 +0800

    chore: setup google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
+import partytown from "@astrojs/partytown";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
